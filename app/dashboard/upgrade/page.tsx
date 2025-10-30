6c25c35cc5d872864d121791a1e1ba16e4ecfe27
turtle <trilokrana1tr@gmail.com>
2025-10-30 14:43:49 +0530

    Update page.tsx

--- app/dashboard/upgrade/page.tsx
+++ app/dashboard/upgrade/page.tsx
@@ -27,7 +27,7 @@ export default function Page() {
           onClick={() => (window.location.href = "https://quiz-lovat-kappa.vercel.app")}
           className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded-lg transition duration-200"
         >
-          Go to Google →
+          Go to course →
         </button>
       </div>
     </div>
